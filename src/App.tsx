371dd9b1647d1aa92f7e1eab4d286dd07ae1f750
yukin <yukinon.0103chiba@gmail.com>
2025-04-06 14:57:38 +0900

    <fix>(<setting>):fix base of vite.config.ts from '/DaiCode/' to '/'

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import NotFound from './Page/NotFound/NotFound'
 
 import './App.css'
 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
 
-            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
       </Router>
     </>
